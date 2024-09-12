ae0de1c3ec04abfbfd9d4cd76ef7da8b3ceff77b
Diego-Esquivias <desqui048@west-mec.org>
2024-09-12 15:10:40 -0700

    notes

--- server.js
+++ server.js
@@ -1,3 +1,30 @@
 var express = require('express');
 var app = express();
-const PORT = 500;
+const PORT = 5000;
+
+// Imported Data
+const peopleData = require('./data/people');
+
+// Set the view engine to EJS
+app.set('view engine', 'ejs');
+
+// Use res.render to load up an ejs view file
+
+// Index page
+app.get("/", function(req, res) {
+    // let people = []
+    let judgement = "Nerds will inherit the earth"
+    res.render('pages/index', {
+        people: peopleData,
+        tagline: judgement,
+    })
+})
+
+// About page
+app.get('/about', function(req, res) {
+    res.render('pages/about')
+})
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+})
